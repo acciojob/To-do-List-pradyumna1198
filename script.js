74e17887a0dc534958aeb29a5c8c752e0294e255
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-11-18 15:24:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const newTodoInput = document.getElementById('newTodoInput');
+  const addTodoBtn = document.getElementById('addTodoBtn');
+  const todoList = document.getElementById('todoList');
 
+  addTodoBtn.addEventListener('click', function () {
+    const todoText = newTodoInput.value.trim(); // Trim removes leading and trailing whitespaces
+
+    if (todoText !== '') {
+      const newTodoItem = document.createElement('li');
+      newTodoItem.textContent = todoText;
+      todoList.appendChild(newTodoItem);
+
+      // Clear input after adding todo
+      newTodoInput.value = '';
+    } else {
+      alert('Please enter a non-empty todo item.');
+    }
+  });
+});
